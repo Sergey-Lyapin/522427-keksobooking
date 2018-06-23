911af5d14b54cdca6c0811bfba69802451fa5f96
Sergey-Lyapin <sergeyl9pin@gmail.com>
2018-06-23 14:51:55 +0300

    Задание по валидации

--- js/map.js
+++ js/map.js
@@ -295,3 +295,53 @@ function createPopupPhoto(photo) {
 
   return popupPhoto;
 }
+
+var timeInField = document.querySelector('#timein');
+var timeOutField = document.querySelector('#timeout');
+var apartmentTypeField = document.querySelector('#type');
+var priceField = document.querySelector('#price');
+var roomNumberField = document.querySelector('#room_number');
+var capacityField = document.querySelector('#capacity');
+
+var typePriceDependency = {
+  bungalo: '0',
+  flat: '1000',
+  house: '5000',
+  palace: '10000'
+};
+
+roomsGuestValidation();
+setMinimalPrice();
+
+function setMinimalPrice() {
+  priceField.min = typePriceDependency[apartmentTypeField.value];
+  priceField.placeholder = typePriceDependency[apartmentTypeField.value];
+}
+
+function roomsGuestValidation() {
+  if ((roomNumberField.value === '1') && (capacityField.value !== '1')) {
+    capacityField.setCustomValidity('В одной комнате может поселиться только один гость.');
+  } else if ((roomNumberField.value === '2') && (capacityField.value !== '1') && (capacityField.value !== '2')) {
+    capacityField.setCustomValidity('В двух комнатах не может поселиться более 2 гостей.');
+  } else if ((roomNumberField.value === '3') && (capacityField.value !== '1') && (capacityField.value !== '2') && (capacityField.value !== '3')) {
+    capacityField.setCustomValidity('В двух комнатах не может поселиться более 3 гостей.');
+  } else if ((roomNumberField.value === '100') && (capacityField.value !== '0')) {
+    capacityField.setCustomValidity('Сто комнат предназначены не для гостей!');
+  } else {
+    capacityField.setCustomValidity('');
+  }
+}
+
+function syncTimeOut() {
+  timeInField.value = timeOutField.value;
+}
+
+function syncTimeIn() {
+  timeOutField.value = timeInField.value;
+}
+
+apartmentTypeField.addEventListener('change', setMinimalPrice);
+capacityField.addEventListener('change', roomsGuestValidation);
+roomNumberField.addEventListener('change', roomsGuestValidation);
+timeInField.addEventListener('change', syncTimeIn);
+timeOutField.addEventListener('change', syncTimeOut);
