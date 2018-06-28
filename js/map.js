d95a28cdbeeac6d394207ed2ebe857995f76b811
Sergey-Lyapin <sergeyl9pin@gmail.com>
2018-06-28 03:29:44 +0300

    Разбил на модули5

--- js/map.js
+++ js/map.js
@@ -226,13 +226,9 @@
 
     mapPins.appendChild(fragment);
   }
-  
   // Устанавливаем начальные координаты метки
-  
   inputAddress.setAttribute('value', (PIN_MAIN_X + PIN_MAIN_WIDTH / 2) + ', ' + (PIN_MAIN_Y + PIN_MAIN_HEIGHT - PIN_POINT_GAP));
-  
   // Описываем передвижение главной метки по карте
-  
   pinMain.addEventListener('mousedown', onMouseDown);
 
   function onMouseDown(evt) {
@@ -274,9 +270,7 @@
     document.addEventListener('mousemove', onMouseMove);
     document.addEventListener('mouseup', onMouseUp);
   }
-  
   // Функция, описывающая переход в активное состояние после перемещения метки
-  
   function onPinmainMouseup() {
     var formFieldset = document.querySelectorAll('form fieldset');
     var formSelect = document.querySelectorAll('form select');
