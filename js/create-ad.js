77427270ab9401ca3dff4c721a5c0a77632b6f12
Sergey-Lyapin <sergeyl9pin@gmail.com>
2018-06-29 04:58:03 +0300

    Разделение на модули

---
+++ js/create-ad.js
@@ -0,0 +1,28 @@
+(function () {
+  function createAd(adArrayElement) {
+    var adElement = adTemplate.cloneNode(true);
+    var adAvatar = adElement.querySelector('.popup__avatar');
+    var closeAdButton = adElement.querySelector('.popup__close');
+
+    adAvatar.src = adArrayElement.author.avatar;
+    adAvatar.alt = adArrayElement.offer.title;
+
+    adElement.querySelector('.popup__title').textContent = adArrayElement.offer.title;
+    adElement.querySelector('.popup__text--address').textContent = adArrayElement.offer.address;
+    adElement.querySelector('.popup__text--price').textContent = adArrayElement.offer.price + '₽/ночь';
+    adElement.querySelector('.popup__type').textContent = window.util.translateType(adArrayElement.offer.type);
+    adElement.querySelector('.popup__text--capacity').textContent = 'Для ' + adArrayElement.offer.guests + ' гостей в ' + adArrayElement.offer.rooms + ' комнатах';
+    adElement.querySelector('.popup__text--time').textContent = 'заезд после ' + adArrayElement.offer.checkin + ', выезд до ' + adArrayElement.offer.checkout;
+    window.util.removeChilds(adElement.querySelector('.popup__features'));
+    adElement.querySelector('.popup__features').appendChild(generateIconsFeatures(adArrayElement.offer.features));
+    adElement.querySelector('.popup__description').textContent = adArrayElement.offer.description;
+    window.util.removeChilds(adElement.querySelector('.popup__photos'));
+    adElement.querySelector('.popup__photos').appendChild(generatePopupPhotos(adArrayElement.offer.photos));
+    adElement.querySelector('.popup__description').textContent = adArrayElement.offer.description;
+    closeAdButton.addEventListener('click', closeAd);
+    closeAdButton.addEventListener('keydown', onPopupCloseEnterPress);
+    document.addEventListener('keydown', onPopupEscPress);
+
+    return adElement;
+  }
+})();
--- js/map.js
+++ js/map.js
@@ -1,17 +1,10 @@
 'use strict';
 
 (function () {
-  var NUMBER_OF_USERS = 8;
   var MIN_X = 0;
   var MAX_X = 1199;
   var MIN_Y = 130;
   var MAX_Y = 630;
-  var MIN_PRICE = 1000;
-  var MAX_PRICE = 1000000;
-  var NUMBER_OF_ROOMS_MIN = 1;
-  var NUMBER_OF_ROOMS_MAX = 5;
-  var GUESTS_MIN = 1;
-  var GUESTS_MAX = 20;
   var PIN_HEIGHT = 70;
   var PIN_WIDTH = 50;
   var PIN_MAIN_WIDTH = 62;
@@ -23,86 +16,18 @@
   var PIN_POINT_GAP = 53;
   var ESC_KEYCODE = 27;
   var ENTER_KEYCODE = 13;
-  var ROOM_TYPES = ['palace', 'flat', 'house', 'bungalo'];
-  var TIMES = ['12:00', '13:00', '14:00'];
-  var ROOM_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-  var ROOM_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-  var AD_TITLES = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
 
   var tokioMap = document.querySelector('.map');
   var pinMain = tokioMap.querySelector('.map__pin--main');
   var adForm = document.querySelector('.ad-form');
   var inputAddress = document.querySelector('#address');
-  var ads = generateAds();
+  var ads = window.generateAds();
   var pinTemplate = document.querySelector('template').content.querySelector('.map__pin');
   var adTemplate = document.querySelector('template').content.querySelector('.map__card');
 
   // Создаём и вставляем карточку объявления
 
-  function generateAds() {
-    var adsArray = [];
 
-    for (var i = 0; i < NUMBER_OF_USERS; i++) {
-      var locationX = window.util.getRandomNumber(MIN_X, MAX_X);
-      var locationY = window.util.getRandomNumber(MIN_Y, MAX_Y);
-
-      adsArray.push({
-        'author': {
-          'avatar': generateAvatars()[i]
-        },
-
-        'offer': {
-          'title': AD_TITLES[i],
-          'address': locationX + ', ' + locationY,
-          'price': window.util.getRandomNumber(MIN_PRICE, MAX_PRICE),
-          'type': window.util.getRandomElement(ROOM_TYPES),
-          'rooms': window.util.getRandomNumber(NUMBER_OF_ROOMS_MIN, NUMBER_OF_ROOMS_MAX),
-          'guests': window.util.getRandomNumber(GUESTS_MIN, GUESTS_MAX),
-          'checkin': window.util.getRandomElement(TIMES),
-          'checkout': window.util.getRandomElement(TIMES),
-          'features': window.util.getRandomArray(ROOM_FEATURES),
-          'description': '',
-          'photos': ROOM_PHOTOS.sort(window.util.compareRandom)
-        },
-
-        'location': {
-          'x': locationX,
-          'y': locationY
-        }
-
-      });
-
-    }
-
-    return adsArray;
-  }
-
-  function createAd(adArrayElement) {
-    var adElement = adTemplate.cloneNode(true);
-    var adAvatar = adElement.querySelector('.popup__avatar');
-    var closeAdButton = adElement.querySelector('.popup__close');
-
-    adAvatar.src = adArrayElement.author.avatar;
-    adAvatar.alt = adArrayElement.offer.title;
-
-    adElement.querySelector('.popup__title').textContent = adArrayElement.offer.title;
-    adElement.querySelector('.popup__text--address').textContent = adArrayElement.offer.address;
-    adElement.querySelector('.popup__text--price').textContent = adArrayElement.offer.price + '₽/ночь';
-    adElement.querySelector('.popup__type').textContent = window.util.translateType(adArrayElement.offer.type);
-    adElement.querySelector('.popup__text--capacity').textContent = 'Для ' + adArrayElement.offer.guests + ' гостей в ' + adArrayElement.offer.rooms + ' комнатах';
-    adElement.querySelector('.popup__text--time').textContent = 'заезд после ' + adArrayElement.offer.checkin + ', выезд до ' + adArrayElement.offer.checkout;
-    window.util.removeChilds(adElement.querySelector('.popup__features'));
-    adElement.querySelector('.popup__features').appendChild(generateIconsFeatures(adArrayElement.offer.features));
-    adElement.querySelector('.popup__description').textContent = adArrayElement.offer.description;
-    window.util.removeChilds(adElement.querySelector('.popup__photos'));
-    adElement.querySelector('.popup__photos').appendChild(generatePopupPhotos(adArrayElement.offer.photos));
-    adElement.querySelector('.popup__description').textContent = adArrayElement.offer.description;
-    closeAdButton.addEventListener('click', closeAd);
-    closeAdButton.addEventListener('keydown', onPopupCloseEnterPress);
-    document.addEventListener('keydown', onPopupEscPress);
-
-    return adElement;
-  }
 
   // Создаем иконки удобств
 
@@ -127,17 +52,7 @@
 
   // Создаем список аватаров для объявления
 
-  function generateAvatars() {
-    var listAvatars = [];
 
-    for (var i = 1; i < NUMBER_OF_USERS + 1; i++) {
-      i = '0' + i;
-      var avatar = 'img/avatars/user' + i + '.png';
-      listAvatars.push(avatar);
-    }
-
-    return listAvatars;
-  }
 
   // Создаем верстку для изображений
 
